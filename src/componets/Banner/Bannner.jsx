40f7df27c1cea1bb97bca1c3f7a5aa39fd8a380a
mazharulislam-12 <505mazharulislam@gmail.com>
2023-09-22 19:33:39 -0700

    banner page desing and card design

--- src/componets/Banner/Bannner.jsx
+++ src/componets/Banner/Bannner.jsx
@@ -2,7 +2,17 @@
 const Bannner = () => {
     return (
         <div>
-            <h2 className="text-center text-5xl">Home page Banner</h2>
+            <div className="flex  items-center bg-[#a7daef] rounded-md">
+                <div className="flex-1 p-9">
+                    <h2 className="text-6xl font-semibold">One Step <br /> Closer To Your <br /> <span className="text-[#7E90FE]">Dream Job</span> </h2>
+                    <p className="my-3">Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+
+                <div className="flex-1">
+                    <img src="assets/images/user.png" alt=""></img>
+                </div>
+            </div>
         </div>
     );
 };
--- src/componets/CatogoryList/CatogoryList.jsx
+++ src/componets/CatogoryList/CatogoryList.jsx
@@ -3,10 +3,41 @@ const CatogoryList = () => {
     return (
         <div>
             <div className="text-center">
-            <h2 className="text-5xl">Job Category List</h2>
+            <h2 className="text-5xl my-5">Job Category List</h2>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
+
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-10 gap-6 mb-10">
+                <div className="bg-[#7E90FE0D] p-8 rounded-md shadow-md">
+                <img src="assets/logo/airbnb.png" alt=""></img>
+                <h3 className="text-xl font-medium my-3">Account & Finance</h3>
+                <p><small>300 Jobs Available</small></p>
+                </div>
+
+                <div className="bg-[#7E90FE0D] p-8 rounded-md shadow-md">
+                <img src="assets/logo/tesla.png" alt=""></img>
+                <h3 className="text-xl font-medium my-3">Account & Finance</h3>
+                <p><small>100+ Jobs Available</small></p>
+                </div>
+
+                <div className="bg-[#7E90FE0D] p-8 rounded-md shadow-md">
+                <img src="assets/logo/google.png" alt=""></img>
+                <h3 className="text-xl font-medium my-3">Account & Finance</h3>
+                <p><small>150 Jobs Available</small></p>
+                </div>
+
+                <div className="bg-[#7E90FE0D] p-8 rounded-md shadow-md">
+                <img src="assets/logo/netflix.png" alt=""></img>
+                <h3 className="text-xl font-medium my-3">Account & Finance</h3>
+                <p><small>224 Jobs Available</small></p>
+                </div>
+            </div>
+            
         </div>
+
+            
+
     );
 };
 
